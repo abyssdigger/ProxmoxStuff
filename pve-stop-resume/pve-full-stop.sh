ff67d2a546d07e50ae9d210694801e464334e6c1
abyssdigger <abyssdigger@gmail.com>
2024-10-10 19:34:22 +0300

    pve-full-stop.sh ready

--- pve-stop-resume/pve-full-stop.sh
+++ pve-stop-resume/pve-full-stop.sh
@@ -1,14 +1,55 @@
 #!/bin/bash
 
-pvescheduler stop
-systemctl stop corosync
-systemctl stop {pveproxy,pvedaemon,pvestatd}
-qm list --full
-qm list | while read f1 f2 f3 f4 f5 f6; do if [[ "$f3" == "running" ]]; then qm suspend $f1 --todisk; fi; done
-systemctl stop pve-firewall
-systemctl stop pve-ha-crm
-systemctl stop pve-ha-lrm
-systemctl stop pve-cluster
-umount /mnt/pve/raid5
+function log_command() { # <command> <prefix> <suppress comand text>
+	echo -n "$2"; [[ "$3" == "" ]] && echo -n "[$1]"; echo -n "..."
+	OUTPUT=$(eval "$1" 2>&1)
+	RES=$?
+	if [[ "$RES" -eq 0 ]]; then
+		echo "ok."
+	else
+		echo "error($RES), output:"
+		echo "$OUTPUT"
+	fi
+	return $RES
+}
 
+echo "== Stopping scheduler, corosync and interactive services... ===================="
 
+log_command "pvescheduler stop"
+
+for srv in corosync pveproxy pvedaemon pvestatd; do
+	log_command "systemctl stop $srv"
+done
+
+echo "== Suspending/stopping VMs... =================================================="
+
+VMLIST=$(qm list --full | grep running)
+echo "$VMLIST"
+echo "$VMLIST" | while read f1 f2 f3 f4 f5 f6; do 
+	if [[ ! "$f1" == "" ]]; then 
+		echo "[VM#$f1]-----------------------------------"
+		log_command "pgrep -f 'task .*:vzdump:$f1:.*' |  xargs -r kill" "...Kill backup tasks (if any)" "suppress"
+		log_command "qm unlock $f1" "........Unlock"
+
+		NEVER_SUSPEND_TAG=$(qm config $f1 | grep -E '^tags: *(|.+;)never-suspend(|;.+)$')
+		if [[ ${#NEVER_SUSPEND_TAG} -eq 0 ]]; then
+			log_command "qm suspend $f1 --todisk" "HYBERNATE: "; res=$?
+		else
+			log_command "qm shutdown $f1" "SHUTDOWN: "; res=$?
+		fi
+		if [[ "$res" -ne 0 ]]; then
+			log_command "qm stop $f1 -overrule-shutdown 1" "ERROR(STOP): "; res=$?
+		fi
+	fi;
+done
+
+echo "== Stopping pve main services... ==============================================="
+
+for srv in pve-firewall pve-ha-crm pve-ha-lrm pve-cluster; do
+	log_command "systemctl stop $srv"
+done
+
+echo "== Setting write cache to 0%... ================================================"
+log_command "echo 0 | tee /proc/sys/vm/dirty_ratio" "WRITE-CACHE-DISABLE: "
+
+echo "== Done ========================================================================"
--- pve-stop-resume/pve-resume.sh
+++ pve-stop-resume/pve-resume.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+echo 40 | tee /proc/sys/vm/dirty_ratio
 
-mount /dev/sdb1 /mnt/pve/raid5
 systemctl start pve-cluster
 systemctl start pve-ha-lrm
 systemctl start pve-ha-crm
 systemctl start pve-firewall
-qm list | while read f1 f2 f3 f4 f5 f6; do if [[ ! "$f1" == "9999" ]]; then qm start $f1; fi; done
+echo ---------
+qm list | while read f1 f2 f3 f4 f5 f6; do 
+	if [[ "$f1" == "VMID" ]]; then 
+		continue; 
+	fi
+	if [[ ! "$f1" == "9999" ]]; then 
+		qm start $f1; 
+	fi; 
+done
 qm list --full
 systemctl start {pveproxy,pvedaemon,pvestatd}
 systemctl start corosync
